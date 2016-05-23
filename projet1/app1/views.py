06ccfc772ac376c3323155f16d9e8dcc45d1e601
tbagrel1 <tomsb07@gmail.com>
2016-05-23 14:41:35 +0200

    coicohdsf

--- projet1/app1/views.py
+++ projet1/app1/views.py
@@ -2,22 +2,27 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from os import getcwd, chdir
 
 def home(request):
-    c = "Coucou !"
-    return HttpResponse(c)
+    return render(request, 'app1/base.html',
+                  {'titre': "message",
+                   'body': "Coucou !"})
 
 def calc(request):
     nombre1 = float(request.GET.get("nombre1", "0"))
     nombre2 = float(request.GET.get("nombre2", "1"))
     resultat = nombre1 + nombre2
-    return render(request, 'app1/calc.html', {'nombre1': nombre1, 'nombre2': nombre2, 'resultat': resultat})
+    return render(request, 'app1/calc.html',
+                  {'nombre1': nombre1,
+                   'nombre2': nombre2,
+                   'resultat': resultat})
 
 def perso(request):
     titre = request.GET.get("titre", "Perso")
     message = request.GET.get("message", "Tu est très gentil(le) !")
-    return render(request, 'app1/base.html', {'titre': titre, 'body': message})
+    return render(request, 'app1/base.html',
+                  {'titre': titre,
+                   'body': message})
 
 def viewScores(request):
     from .func.viewScores import _viewScores
